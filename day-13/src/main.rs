ede17389d37be0213379c1ea3d00b09d08d5225b
0xk1f0 <dev@k1f0.dev>
2023-12-14 01:59:40 +0100

    i have no clue why no work

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -6,72 +6,102 @@ fn part_one() -> usize {
     let input = File::open("input.txt").unwrap();
     // define a new buffer reader and get lines
     let lines: Vec<String> = BufReader::new(input).lines().map(|l| l.unwrap()).collect();
-    // find splits
-    let mut all_splits: Vec<usize> = Vec::new();
-    all_splits.push(0);
-    let _: Vec<_> = lines
-        .iter()
-        .enumerate()
-        .map(|(i, l)| {
-            if l.len() == 0 {
-                all_splits.push(i + 1);
-            }
-        })
-        .collect();
-    all_splits.push(lines.len());
-
+    // collector vector for preventing duplicates
+    let mut collector: Vec<usize> = Vec::new();
     // define a allup sum
     let mut sum: usize = 0;
-
-    // flip flop row col
-    let mut type_row: bool = false;
-    let mut inner_iterate: usize = 0;
-    for i in 0..lines.len() {
-        if all_splits.contains(&i) {
-            let mut current_lines: Vec<String> = lines.clone();
-            current_lines = current_lines.into_iter()
-                .skip(all_splits[inner_iterate])
-                .take(all_splits[inner_iterate + 1] - 1)
-                .collect();
-            if type_row {
-                let _: Vec<_> = current_lines
-                    .iter()
-                    .enumerate()
-                    .map(|(i, l)| {
-                        if i < current_lines.len() - 1 && l == &current_lines[i + 1] {
-                            println!("RMATCH at {}", i + 1);
-                            sum += (i + 1) * 100;
-                        }
-                    })
-                    .collect();
-            } else {
-                for col in 0..current_lines[0].len() {
-                    let mut broken: bool = false;
-                    for row in 0..current_lines.len() + 1 {
-                        if row < current_lines.len() - 1 {
-                            let this_line: Vec<char> = current_lines[col].chars().collect();
-                            let that_line: Vec<char> = current_lines[col].chars().collect();
-                            if this_line[row] != that_line[row + 1] {
-                                broken = true;
-                                break;
-                            } else {
-                                if row > 0 && this_line[row - 1] != that_line[row + 2] {
-                                    broken = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    if broken {
-                        println!("VMATCH at {}", col);
-                        sum += col;
+    // offset for keeping track of position
+    let mut offset: usize = 0;
+    // another iterator for duplicate prevention
+    let mut side_iter: usize = 0;
+    // sum for number of emptylines for col matching
+    let mut empty_lines: usize = 0;
+    // for rows
+    for (i, line) in lines.iter().enumerate() {
+        if line.len() > 0 {
+            if i > offset && line == &lines[i - 1] {
+                let mut match_offset: usize = 1;
+                let mut broken: bool = true;
+                while i + match_offset < lines.len() && &lines[i + match_offset].len() != &0 && &lines[i - (match_offset + 1)].len() != &0 {
+                    if &lines[i + match_offset] != &lines[i - (match_offset + 1)] {
+                        broken = true;
                         break;
                     }
+                    broken = false;
+                    match_offset += 1;
+                }
+                if !broken {
+                    if !collector.contains(&side_iter) {
+                        collector.push(side_iter);
+                        sum += i - offset;
+                    }
+                    continue;
+                }else if &lines[i + match_offset].len() == &0 || &lines[i - (match_offset + 1)].len() == &0 {
+                    if !collector.contains(&side_iter) {
+                        collector.push(side_iter);
+                        sum += i - offset;
+                    }
+                    sum += i - offset;
+                    continue;
                 }
             }
-            type_row = !type_row;
-            inner_iterate += 1;
+        } else {
+            empty_lines += 1;
+            offset = i;
+            side_iter = i + 1;
+            continue;
         }
+    }
+    // for columns
+    offset = 0;
+    let mut iterator: usize = 0;
+    for _x in 0..empty_lines + 1 {
+        let mut matrix: Vec<String> = Vec::new();
+        while lines[iterator].len() > 0 {
+            for i in 0..lines[iterator].len() {
+                if matrix.len() < i + 1 {
+                    matrix.push(String::from(lines[iterator].chars().nth(i).unwrap()));
+                } else {
+                    let mut curr_string = matrix[i].clone();
+                    curr_string.push(lines[iterator].chars().nth(i).unwrap());
+                    matrix[i] = curr_string;
+                }
+            }
+            if iterator < lines.len() - 1 {
+                iterator += 1;
+            } else {
+                break;
+            }
+        }
+        for (i, line) in matrix.iter().enumerate() {
+            if i > 0 && line == &matrix[i - 1] {
+                let mut match_offset: usize = 1;
+                let mut broken: bool = true;
+                while i > match_offset && i < (matrix.len() - (match_offset + 1)) {
+                    if &matrix[i + match_offset] != &matrix[i - (match_offset + 1)] {
+                        broken = true;
+                        break;
+                    }
+                    broken = false;
+                    match_offset += 1;
+                }
+                if !broken {
+                    if !collector.contains(&offset) {
+                        collector.push(offset);
+                        sum += (i - 1) * 100;
+                    }
+                    break;
+                }else if i < match_offset || i >= (matrix.len() - (match_offset + 1)) {
+                    if !collector.contains(&offset) {
+                        collector.push(offset);
+                        sum += (i - 1) * 100;
+                    }
+                    break;
+                }
+            }
+        }
+        offset += matrix[0].len() + 1;
+        iterator += 1;
     }
 
     return sum;
