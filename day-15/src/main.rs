c9df4e735f63335b97eaedc7f2ec27586bb7884a
0xk1f0 <fteppan@protonmail.com>
2023-12-15 15:58:27 +0100

    add basics for day 15 part 2

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -47,18 +47,31 @@ fn part_two() -> usize {
                 }
             }).count();
         } else {
-            let _ = seq.1.iter().map(|&code| {
-                seq.0 = ((seq.0 + code as usize) * 17) % 256
+            let _ = seq.1.iter().take_while(|&x| x != &'=').map(|&code| {
+                seq.0 = ((seq.0 + code as usize) * 17) % 256;
+            }).count();
+            boxes.iter_mut().map(|boxy| {
+                if let Some(x) = boxy.1.iter().position(|x| x.starts_with(&str.split("=").nth(0).unwrap())) {
+                    boxy.1.remove(x);
+                }
             }).count();
             let mut current_box = boxes.get(&seq.0).unwrap_or(&vec![]).clone();
             current_box.insert(0, str);
             boxes.insert(seq.0, current_box);
         }
+        println!("{boxes:?}");
     }).count();
 
-    println!("{boxes:?}");
+    boxes.iter_mut().map(|(k, v)| {
+        let iterator: usize = 1;
+        let sum: usize = v.iter_mut().map(|vec| {
+            let x = (k + 1) * iterator * vec.split("=").nth(1).unwrap().parse::<usize>().unwrap();
+            println!("{} - {}", vec, vec.split("=").nth(1).unwrap().parse::<usize>().unwrap());
+            x
+        }).sum::<usize>();
 
-    0
+        sum
+    }).sum()
 }
 
 fn main() {
