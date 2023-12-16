8565867ab9fad0ce742e4b2ef4e97963aa198651
0xk1f0 <dev@k1f0.dev>
2023-12-16 16:57:20 +0100

    add full day 15 solution

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
 
 fn part_one() -> usize {
     // open our input file
@@ -10,17 +10,23 @@ fn part_one() -> usize {
     // all codes
     let mut ascii_codes: Vec<(usize, Vec<usize>)> = Vec::new();
     for line in lines {
-        ascii_codes = line.split(",").map(|seq| {
-            (0, seq.chars().map(|c| c as usize).collect())
-        }).collect();
+        ascii_codes = line
+            .split(",")
+            .map(|seq| (0, seq.chars().map(|c| c as usize).collect()))
+            .collect();
     }
     // get the total sum of loads
-    ascii_codes.iter_mut().map(|seq| {
-        let _ = seq.1.iter().map(|&code| {
-            seq.0 = ((seq.0 + code) * 17) % 256
-        }).count();
-        seq.0
-    }).sum()
+    ascii_codes
+        .iter_mut()
+        .map(|seq| {
+            let _ = seq
+                .1
+                .iter()
+                .map(|&code| seq.0 = (((seq.0 + code) * 17) % 256) + 1)
+                .count();
+            seq.0
+        })
+        .sum()
 }
 
 fn part_two() -> usize {
@@ -28,50 +34,77 @@ fn part_two() -> usize {
     let input = File::open("input.txt").unwrap();
     // define a new buffer reader and get lines
     let lines: Vec<String> = BufReader::new(input).lines().map(|l| l.unwrap()).collect();
-    // all codes
+    // vector for all codes
     let mut ascii: Vec<(usize, Vec<char>)> = Vec::new();
-    // boxes
-    let mut boxes: HashMap<usize,Vec<String>> = HashMap::new();
+    // boxes hashmap for easy lookup
+    let mut boxes: HashMap<usize, Vec<String>> = HashMap::new();
+    // collect all items to vector
     for line in lines {
-        ascii = line.split(",").map(|seq| {
-            (0, seq.chars().collect())
-        }).collect();
+        ascii = line
+            .split(",")
+            .map(|seq| (0, seq.chars().collect()))
+            .collect();
     }
-    // get the total sum of loads
-    ascii.iter_mut().map(|seq| {
-        let str = String::from_iter(seq.1.iter());
-        if seq.1.contains(&'-') {
-            boxes.iter_mut().map(|boxy| {
-                if let Some(x) = boxy.1.iter().position(|x| x.starts_with(&str.split("-").nth(0).unwrap())) {
-                    boxy.1.remove(x);
-                }
-            }).count();
-        } else {
-            let _ = seq.1.iter().take_while(|&x| x != &'=').map(|&code| {
-                seq.0 = ((seq.0 + code as usize) * 17) % 256;
-            }).count();
-            boxes.iter_mut().map(|boxy| {
-                if let Some(x) = boxy.1.iter().position(|x| x.starts_with(&str.split("=").nth(0).unwrap())) {
-                    boxy.1.remove(x);
-                }
-            }).count();
+    // perform insertion and deletion for all boxes
+    ascii
+        .iter_mut()
+        .map(|seq| {
+            // from a string from the chars
+            let str = String::from_iter(seq.1.iter());
+            // calculate the sequence hashlabel
+            let _ = seq
+                .1
+                .iter()
+                .take_while(|&x| x != &'=' && x != &'-')
+                .map(|&code| {
+                    seq.0 = ((seq.0 + code as usize) * 17) % 256;
+                })
+                .count();
             let mut current_box = boxes.get(&seq.0).unwrap_or(&vec![]).clone();
-            current_box.insert(0, str);
-            boxes.insert(seq.0, current_box);
-        }
-        println!("{boxes:?}");
-    }).count();
-
-    boxes.iter_mut().map(|(k, v)| {
-        let iterator: usize = 1;
-        let sum: usize = v.iter_mut().map(|vec| {
-            let x = (k + 1) * iterator * vec.split("=").nth(1).unwrap().parse::<usize>().unwrap();
-            println!("{} - {}", vec, vec.split("=").nth(1).unwrap().parse::<usize>().unwrap());
-            x
-        }).sum::<usize>();
-
-        sum
-    }).sum()
+            if seq.1.contains(&'-') {
+                // check if box has item
+                if let Some(x) = current_box
+                    .iter()
+                    .position(|x| x.starts_with(&str.split("-").nth(0).unwrap()))
+                {
+                    // remove if true
+                    current_box.remove(x);
+                    
+                }
+                // write back to hashmap
+                boxes.insert(seq.0, current_box);
+            } else {
+                // check if box has item at position
+                if let Some(x) = current_box
+                    .iter()
+                    .position(|x| x.starts_with(&str.split("=").nth(0).unwrap()))
+                {
+                    // insert new item before old
+                    current_box.insert(x, str);
+                    // remove old
+                    current_box.remove(x + 1);
+                } else {
+                    // if box doesnt have item, append new
+                    current_box.push(str);
+                }
+                // write to hashmap
+                boxes.insert(seq.0, current_box);
+            }
+        })
+        .count();
+    // get the focal power sum
+    boxes
+        .iter_mut()
+        .map(|(k, v)| {
+            v.iter_mut()
+                .enumerate()
+                .map(|(i, vec)| {
+                    let x = (k + 1) * (i + 1) * vec.split("=").nth(1).unwrap().parse::<usize>().unwrap();
+                    x
+                })
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 fn main() {
