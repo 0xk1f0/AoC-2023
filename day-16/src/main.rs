2d82928068cf42e62b146cda7eb1c9e06a689588
0xk1f0 <dev@k1f0.dev>
2023-12-17 02:15:26 +0100

    add full solution for day 16

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,13 +1,9 @@
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::{
-    io::{stdout, Write},
-    thread::sleep,
-    time::Duration,
-};
 
-#[derive(PartialEq, Copy, Clone, Debug)]
+// directions for easier matching
+#[derive(PartialEq, Copy, Clone, Debug, Hash, Eq)]
 enum Direction {
     UP,
     DOWN,
@@ -16,7 +12,195 @@ enum Direction {
     OUTSIDE,
 }
 
-fn direct_beam(beam: &mut (usize, usize, Direction)) {}
+fn trace_path(matrix: &Vec<Vec<char>>, x: usize, y: usize, direction: Direction) -> usize {
+    // all beams
+    let mut beams_list: Vec<(usize, usize, Direction)> = Vec::new();
+    // all activated element cords
+    let mut activated_list: HashSet<(usize, usize)> = HashSet::new();
+    // all visited elements with direction included
+    let mut traversed_list: HashSet<(usize, usize, Direction)> = HashSet::new();
+    // instantiate a new beam
+    beams_list.push((x, y, direction));
+    // activate it
+    activated_list.insert((x, y));
+    // initiator vector to match first element
+    let mut initiator: bool = true;
+    // walk the beams until there are none left
+    while beams_list.len() != 0 {
+        // new beam initiator
+        let mut new_beams: Vec<(usize, usize, Direction)> = Vec::new();
+        beams_list
+            .iter_mut()
+            .map(|beam| {
+                // checks for early pathing exit
+                let mut activated = false;
+                let mut re_check = true;
+                // match beam direction
+                match beam.2 {
+                    Direction::UP => {
+                        if (beam.1 - 1) > 1000 {
+                            *beam = (beam.0, beam.1, Direction::OUTSIDE);
+                            return;
+                        }
+                        if traversed_list.contains(&beam) {
+                            activated = true;
+                        }
+                        if !initiator {
+                            *beam = (beam.0, beam.1 - 1, beam.2);
+                        } else {
+                            activated = true;
+                        }
+                        match matrix[beam.1][beam.0] {
+                            '.' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '-' => {
+                                *beam = (beam.0, beam.1, Direction::RIGHT);
+                                new_beams.push((beam.0, beam.1, Direction::LEFT));
+                                re_check = false; // dont early exit
+                            }
+                            '|' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '/' => {
+                                *beam = (beam.0, beam.1, Direction::RIGHT);
+                            }
+                            '\\' => {
+                                *beam = (beam.0, beam.1, Direction::LEFT);
+                            }
+                            _ => panic!("Unexpected Direction"),
+                        }
+                    }
+                    Direction::RIGHT => {
+                        if (beam.0 + 1) > matrix[beam.1].len() - 1 {
+                            *beam = (beam.0, beam.1, Direction::OUTSIDE);
+                            return;
+                        }
+                        if traversed_list.contains(&beam) {
+                            activated = true;
+                        }
+                        if !initiator {
+                            *beam = (beam.0 + 1, beam.1, beam.2);
+                        } else {
+                            activated = true;
+                        }
+                        match matrix[beam.1][beam.0] {
+                            '.' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '-' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '|' => {
+                                *beam = (beam.0, beam.1, Direction::UP);
+                                new_beams.push((beam.0, beam.1, Direction::DOWN));
+                                re_check = false;
+                            }
+                            '/' => {
+                                *beam = (beam.0, beam.1, Direction::UP);
+                            }
+                            '\\' => {
+                                *beam = (beam.0, beam.1, Direction::DOWN);
+                            }
+                            _ => panic!("Unexpected Direction"),
+                        }
+                    }
+                    Direction::DOWN => {
+                        if (beam.1 + 1) > matrix.len() - 1 {
+                            *beam = (beam.0, beam.1, Direction::OUTSIDE);
+                            return;
+                        }
+                        if traversed_list.contains(&beam) {
+                            activated = true;
+                        }
+                        if !initiator {
+                            *beam = (beam.0, beam.1 + 1, beam.2);
+                        } else {
+                            activated = true;
+                        }
+                        match matrix[beam.1][beam.0] {
+                            '.' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '-' => {
+                                *beam = (beam.0, beam.1, Direction::LEFT);
+                                new_beams.push((beam.0, beam.1, Direction::RIGHT));
+                                re_check = false;
+                            }
+                            '|' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '/' => {
+                                *beam = (beam.0, beam.1, Direction::LEFT);
+                            }
+                            '\\' => {
+                                *beam = (beam.0, beam.1, Direction::RIGHT);
+                            }
+                            _ => panic!("Unexpected Direction"),
+                        }
+                    }
+                    Direction::LEFT => {
+                        if (beam.0 - 1) > 1000 {
+                            *beam = (beam.0, beam.1, Direction::OUTSIDE);
+                            return;
+                        }
+                        if traversed_list.contains(&beam) {
+                            activated = true;
+                        }
+                        if !initiator {
+                            *beam = (beam.0 - 1, beam.1, beam.2);
+                        } else {
+                            activated = true;
+                        }
+                        match matrix[beam.1][beam.0] {
+                            '.' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '-' => {
+                                *beam = (beam.0, beam.1, beam.2);
+                            }
+                            '|' => {
+                                *beam = (beam.0, beam.1, Direction::UP);
+                                new_beams.push((beam.0, beam.1, Direction::DOWN));
+                                re_check = false;
+                            }
+                            '/' => {
+                                *beam = (beam.0, beam.1, Direction::DOWN);
+                            }
+                            '\\' => {
+                                *beam = (beam.0, beam.1, Direction::UP);
+                            }
+                            _ => panic!("Unexpected Direction"),
+                        }
+                    }
+                    Direction::OUTSIDE => {}
+                }
+                // if both past and current beam tiles are activated
+                // we can early exit
+                if re_check && activated && traversed_list.contains(&beam) {
+                    *beam = (beam.0, beam.1, Direction::OUTSIDE);
+                    return;
+                } else {
+                    // if we dont seem to have visited these tiles
+                    // add them to traversed and activated
+                    activated_list.insert((beam.0, beam.1));
+                    traversed_list.insert(*beam);
+                }
+                // set the initator to false
+                // only relevant on first run
+                initiator = false;
+            })
+            .count();
+        // add the new beam
+        for beam in new_beams {
+            beams_list.push(beam);
+        }
+        // filter out expired beams
+        beams_list.retain(|beam| beam.2 != Direction::OUTSIDE);
+    }
+    // return the length of activated items
+    activated_list.len()
+}
 
 fn part_one() -> usize {
     // open our input file
@@ -25,140 +209,52 @@ fn part_one() -> usize {
     let lines: Vec<String> = BufReader::new(input).lines().map(|l| l.unwrap()).collect();
     // define a matrix
     let mut matrix: Vec<Vec<char>> = Vec::new();
-    // all beams
-    let mut beams_list: Vec<(usize, usize, Direction)> = Vec::new();
-    // all activated elements corsd
-    let mut activated_list: HashSet<(usize, usize)> = HashSet::new();
     // iterate lines and add to matrix
     for line in lines {
         matrix.push(line.chars().collect());
     }
-    // instantiate a new beam
-    beams_list.push((0, 0, Direction::DOWN));
-    activated_list.insert((0, 0));
-    // loop protect
-    let mut loop_protect = 0;
     // walk the beams
-    while loop_protect < 800 && beams_list.len() != 0 {
-        loop_protect += 1;
-        let mut new_beams: Vec<(usize, usize, Direction)> = Vec::new();
-        beams_list
-            .iter_mut()
-            .map(|beam| match beam.2 {
-                Direction::UP => {
-                    if (beam.1 - 1) > 1000 {
-                        *beam = (beam.0, beam.1, Direction::OUTSIDE);
-                        return;
-                    }
-                    *beam = (beam.0, beam.1 - 1, beam.2);
-                    match matrix[beam.1][beam.0] {
-                        '.' => {
-                            *beam = (beam.0, beam.1, beam.2);
-                        }
-                        '-' => {
-                            *beam = (beam.0, beam.1, Direction::RIGHT);
-                            new_beams.push((beam.0, beam.1, Direction::LEFT));
-                        }
-                        '|' => *beam = (beam.0, beam.1, beam.2),
-                        '/' => {
-                            *beam = (beam.0, beam.1, Direction::RIGHT);
-                        }
-                        '\\' => {
-                            *beam = (beam.0, beam.1, Direction::LEFT);
-                        }
-                        _ => panic!("Unexpected Direction"),
-                    }
-                    activated_list.insert((beam.0, beam.1));
-                }
-                Direction::RIGHT => {
-                    if (beam.0 + 1) > matrix[beam.1].len() - 1 {
-                        *beam = (beam.0, beam.1, Direction::OUTSIDE);
-                        return;
-                    }
-                    *beam = (beam.0 + 1, beam.1, beam.2);
-                    match matrix[beam.1][beam.0] {
-                        '.' => {
-                            *beam = (beam.0, beam.1, beam.2);
-                        }
-                        '-' => *beam = (beam.0, beam.1, beam.2),
-                        '|' => {
-                            *beam = (beam.0, beam.1, Direction::UP);
-                            new_beams.push((beam.0, beam.1, Direction::DOWN));
-                        }
-                        '/' => {
-                            *beam = (beam.0, beam.1, Direction::UP);
-                        }
-                        '\\' => {
-                            *beam = (beam.0, beam.1, Direction::DOWN);
-                        }
-                        _ => panic!("Unexpected Direction"),
-                    }
-                    activated_list.insert((beam.0, beam.1));
-                }
-                Direction::DOWN => {
-                    if (beam.1 + 1) > matrix.len() - 1 {
-                        *beam = (beam.0, beam.1, Direction::OUTSIDE);
-                        return;
-                    }
-                    *beam = (beam.0, beam.1 + 1, beam.2);
-                    match matrix[beam.1][beam.0] {
-                        '.' => {
-                            *beam = (beam.0, beam.1, beam.2);
-                        }
-                        '-' => {
-                            *beam = (beam.0, beam.1, Direction::LEFT);
-                            new_beams.push((beam.0, beam.1, Direction::RIGHT));
-                        }
-                        '|' => *beam = (beam.0, beam.1, beam.2),
-                        '/' => {
-                            *beam = (beam.0, beam.1, Direction::LEFT);
-                        }
-                        '\\' => {
-                            *beam = (beam.0, beam.1, Direction::RIGHT);
-                        }
-                        _ => panic!("Unexpected Direction"),
-                    }
-                    activated_list.insert((beam.0, beam.1));
-                }
-                Direction::LEFT => {
-                    if (beam.0 - 1) > 1000 {
-                        *beam = (beam.0, beam.1, Direction::OUTSIDE);
-                        return;
-                    }
-                    *beam = (beam.0 - 1, beam.1, beam.2);
-                    match matrix[beam.1][beam.0] {
-                        '.' => {
-                            *beam = (beam.0, beam.1, beam.2);
-                        }
-                        '-' => *beam = (beam.0, beam.1, beam.2),
-                        '|' => {
-                            *beam = (beam.0, beam.1, Direction::UP);
-                            new_beams.push((beam.0, beam.1, Direction::DOWN));
-                        }
-                        '/' => {
-                            *beam = (beam.0, beam.1, Direction::DOWN);
-                        }
-                        '\\' => {
-                            *beam = (beam.0, beam.1, Direction::UP);
-                        }
-                        _ => panic!("Unexpected Direction"),
-                    }
-                    activated_list.insert((beam.0, beam.1));
-                }
-                Direction::OUTSIDE => {}
-            })
-            .count();
-        // add the new beam
-        if new_beams.len() > 0 {
-            for beam in new_beams {
-                beams_list.push(beam);
+    trace_path(&matrix, 0, 0, Direction::RIGHT)
+}
+
+fn part_two() -> usize {
+    // open our input file
+    let input = File::open("input.txt").unwrap();
+    // define a new buffer reader and get lines
+    let lines: Vec<String> = BufReader::new(input).lines().map(|l| l.unwrap()).collect();
+    // define a matrix
+    let mut matrix: Vec<Vec<char>> = Vec::new();
+    // check all sum
+    let mut sum = 0;
+    // iterate lines and add to matrix
+    for line in lines {
+        matrix.push(line.chars().collect());
+    }
+    // walk start and end of each row
+    for row in 0..matrix.len() {
+        for column in [0, matrix[0].len() - 1] {
+            if row == 0 {
+                sum = sum.max(trace_path(&matrix, column, row, Direction::RIGHT));
+            } else {
+                sum = sum.max(trace_path(&matrix, column, row, Direction::LEFT));
             }
         }
-        beams_list.retain(|beam| beam.2 != Direction::OUTSIDE);
     }
-    activated_list.len()
+    // walk start and end of each column
+    for column in 0..matrix[0].len() {
+        for row in [0, matrix.len() - 1] {
+            if row == 0 {
+                sum = sum.max(trace_path(&matrix, column, row, Direction::DOWN));
+            } else {
+                sum = sum.max(trace_path(&matrix, column, row, Direction::UP));
+            }
+        }
+    }
+    // return the max sum
+    sum
 }
 
 fn main() {
     println!("Part 1> The Sum of Energized Elements is: {}", part_one());
+    println!("Part 2> The Highest Sum is: {}", part_two());
 }
